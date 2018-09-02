bd788ec2b057b5e1351b3ab1f23b4aa7b9fd07f2
adam <chenxuannian@gmail.com>
2018-09-02 20:59:01 +0800

    update model to adapt unknown width

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ from keras import models
 from sliding_window_layer import SlidingWindowLayer
 from keras.layers.wrappers import TimeDistributed
 import keras.backend as K
+import numpy as np
+import tensorflow as tf
 
 
 def ctc_lambda_func(args):
@@ -11,10 +13,40 @@ def ctc_lambda_func(args):
     return K.ctc_batch_cost(y_true, y_pred, input_length, label_length)
 
 
-def kale(input_shape=(32, 280, 1), num_classes=5991, max_string_len=10):
+def sliding_window_layer(inputs, character_width=32, character_step=8):
+    # inputs: batches*32*280*1
+
+    for b in range(inputs.shape[0]):
+        batch_input = inputs[b, :, :, :].reshape((1, inputs.shape[1], inputs.shape[2], inputs.shape[3]))
+        for w in range(0, batch_input.shape[2] - character_width, character_step):
+            if w == 0:
+                output_batch = batch_input[:, :, w:(w + 1) * character_width, :]
+            else:
+                output_batch = np.concatenate((output_batch, batch_input[:, :, w:w + character_width, :]), axis=0)
+
+        if b == 0:
+            output = output_batch
+        else:
+            output = np.concatenate((output, output_batch), axis=0)
+    return output
+
+
+def swl_lambda_func(input):
+    swl_output = tf.py_func(sliding_window_layer, [input], tf.float32)
+    return swl_output
+
+
+def kale(input_shape=(32, None, 1), num_classes=5991, max_string_len=10):
     input = layers.Input(shape=input_shape)
-    swl_output = SlidingWindowLayer()(input)
-    x = TimeDistributed(layers.Conv2D(50, (3, 3), padding='same', activation='relu', name='conv1'))(swl_output)
+    # swl_output = tf.py_func(sliding_window_layer, [input], tf.float32)
+    swl_output = layers.Lambda(swl_lambda_func)(input)
+    xx = layers.Reshape((-1, 32, 32, 1))(swl_output)
+    print(xx.get_shape())
+    print(xx.shape)
+    xx._keras_shape = tuple(xx.get_shape().as_list())
+    print(K.int_shape(xx))
+    # swl_output = SlidingWindowLayer()(input)
+    x = TimeDistributed(layers.Conv2D(50, (3, 3), padding='same', activation='relu', name='conv1'))(xx)
     x = TimeDistributed(layers.BatchNormalization(axis=-1))(x)
     x = TimeDistributed(layers.Conv2D(100, (3, 3), padding='same', activation='relu', name='conv2'))(x)
     x = TimeDistributed(layers.Dropout(0.1))(x)
@@ -58,7 +90,7 @@ def kale(input_shape=(32, 280, 1), num_classes=5991, max_string_len=10):
     seq_length = layers.Input(name='seq_length', shape=[1], dtype='int64')
     label_length = layers.Input(name='label_length', shape=[1], dtype='int64')
     ctc_output = layers.Lambda(ctc_lambda_func, output_shape=(1,),
-                             name='ctc')([label, classifier_output, seq_length, label_length])
+                               name='ctc')([label, classifier_output, seq_length, label_length])
     model = models.Model(inputs=[input, label, seq_length, label_length], outputs=[ctc_output])
     model.summary()
     return model
